d51958a77e82e69f7b5b33fabdfba19d9d95bd3d
Alexey_Berezin <alexey_berezin@epam.com>
2018-01-23 01:06:49 +0300

    Added declaration files to basic operations: +, -, / and *

--- lib/multiply/index.d.ts
+++ lib/multiply/index.d.ts
@@ -1,1 +1,10 @@
-export default function multiply<T, U>(n: T, m: U): number
+import {
+  CurriedFunction1,
+  CurriedFunction2
+} from '../../misc/types/fn';
+
+declare function multiply<T1, T2>(): CurriedFunction2<T1, T2, number>
+declare function multiply<T1, T2>(t1: T1): CurriedFunction1<T2, number>
+declare function multiply<T1, T2>(t1: T1, t2: T2): number
+
+export = multiply;
