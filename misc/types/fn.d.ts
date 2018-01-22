f3504f85c9fc28a6169631765b3adc2b6bcece7a
Alexey_Berezin <alexey_berezin@epam.com>
2018-01-23 00:30:45 +0300

    Refactored empty lines

--- misc/types/fn.d.ts
+++ misc/types/fn.d.ts
@@ -3,13 +3,11 @@ interface CurriedFunction1<T1, R> {
   (t1: T1): R;
 }
 
-
 interface CurriedFunction2<T1, T2, R> {
   (): CurriedFunction2<T1, T2, R>;
   (t1: T1): CurriedFunction1<T2, R>;
   (t1: T1, t2: T2): R;
 }
-
 
 interface CurriedFunction3<T1, T2, T3, R> {
   (): CurriedFunction3<T1, T2, T3, R>;
@@ -17,7 +15,6 @@ interface CurriedFunction3<T1, T2, T3, R> {
   (t1: T1, t2: T2): CurriedFunction1<T3, R>;
   (t1: T1, t2: T2, t3: T3): R;
 }
-
 
 interface CurriedFunction4<T1, T2, T3, T4, R> {
   (): CurriedFunction4<T1, T2, T3, T4, R>;
@@ -27,7 +24,6 @@ interface CurriedFunction4<T1, T2, T3, T4, R> {
   (t1: T1, t2: T2, t3: T3, t4: T4): R;
 }
 
-
 interface CurriedFunction5<T1, T2, T3, T4, T5, R> {
   (): CurriedFunction5<T1, T2, T3, T4, T5, R>;
   (t1: T1): CurriedFunction4<T2, T3, T4, T5, R>;
@@ -36,7 +32,6 @@ interface CurriedFunction5<T1, T2, T3, T4, T5, R> {
   (t1: T1, t2: T2, t3: T3, t4: T4): CurriedFunction1<T5, R>;
   (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5): R;
 }
-
 
 interface CurriedFunction6<T1, T2, T3, T4, T5, T6, R> {
   (): CurriedFunction6<T1, T2, T3, T4, T5, T6, R>;
@@ -48,7 +43,6 @@ interface CurriedFunction6<T1, T2, T3, T4, T5, T6, R> {
   (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6): R;
 }
 
-
 interface CurriedFunction7<T1, T2, T3, T4, T5, T6, T7, R> {
   (): CurriedFunction7<T1, T2, T3, T4, T5, T6, T7, R>;
   (t1: T1): CurriedFunction6<T2, T3, T4, T5, T6, T7, R>;
@@ -59,7 +53,6 @@ interface CurriedFunction7<T1, T2, T3, T4, T5, T6, T7, R> {
   (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6): CurriedFunction1<T7, R>;
   (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7): R;
 }
-
 
 interface CurriedFunction8<T1, T2, T3, T4, T5, T6, T7, T8, R> {
   (): CurriedFunction8<T1, T2, T3, T4, T5, T6, T7, T8, R>;
@@ -86,7 +79,6 @@ interface CurriedFunction9<T1, T2, T3, T4, T5, T6, T7, T8, T9, R> {
   (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8): CurriedFunction1<T9, R>;
   (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8, t9: T9): R;
 }
-
 
 interface CurriedFunction10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R> {
   (): CurriedFunction10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R>;
