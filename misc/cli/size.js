8d9ecc3191faed400957914bced394f5ebdeba22
Kelin2025 <kelin2025@yandex.ru>
2018-01-27 01:41:26 +0300

    :speech_balloon: Refactored: size-limit CLI util

--- misc/cli/size.js
+++ misc/cli/size.js
@@ -1,58 +1,92 @@
 const fs = require('fs')
 const path = require('path')
+const util = require('util')
 const chalk = require('chalk')
 const getSize = require('size-limit')
+const minimist = require('minimist')
 
-const getPath = name => path.resolve('lib', name, 'index.js')
+let longestName
+const args = minimist(process.argv.slice(2))
+const createRow = cols => '║ ' + cols.join(' ║ ') + ' ║'
+const formatName = name => chalk.bold(name.padEnd(longestName))
+const formatSize = size => `${size}`.padStart(4) + ' B'
 
-const getSizeAndSave = name =>
-  getSize(getPath(name)).then(size => ({ name, size }))
+Promise
+  // Get lib path
+  .resolve(path.resolve('lib'))
+  // Get list of methods
+  .then(util.promisify(fs.readdir))
+  // Exclude index.js and _internals
+  .then(methods => {
+    const exclude = m => !['index.js', '_internal'].includes(m)
 
-const alphabetically = (a, b) => {
-  const [aLower, bLower] = [a, b].map(i => i.name.toLowerCase())
-  return aLower > bLower ? 1 : aLower == bLower ? 0 : -1
-}
+    return methods.filter(exclude)
+  })
+  // Filter only choosed methods (if passed)
+  .then(methods => {
+    return args._.length ? methods.filter(m => args._.includes(m)) : methods
+  })
+  // Stop if no methods matched
+  .then(methods => {
+    return !methods.length ? Promise.reject(['NO_METHODS']) : methods
+  })
+  // Check size for every method
+  .then(methods => {
+    const getMaxLen = (max, cur) => (cur.length > max ? cur.length : max)
+    longestName = methods.reduce(getMaxLen, 0)
+    const getIndex = name => path.resolve('lib', name, 'index.js')
+    const sizeLimit = name =>
+      getSize(getIndex(name)).then(size => ({ name, size }))
 
-const wrapper = (len, data) =>
-  console.log(`╔${'═'.repeat(len)}╗\n${legend()}\n${data}╚${'═'.repeat(len)}╝`)
+    return Promise.all(methods.map(sizeLimit))
+  })
+  // Sort alphabetically
+  .then(methods => {
+    const byName = (a, b) =>
+      a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
 
-const separator = len => `╟${'─'.repeat(len)}╢\n`
+    return methods.sort(byName)
+  })
+  // Create bordered row
+  .then(methods => {
+    return methods.map(i => ({
+      ...i,
+      border: createRow([formatName(i.name), formatSize(i.size)])
+    }))
+  })
+  // Draw table
+  .then(methods => {
+    const topper = `╔${'═'.repeat(longestName + 2)}╦${'═'.repeat(8)}╗`
+    const hrline = `╟${'─'.repeat(longestName + 2)}╫${'─'.repeat(8)}╢`
+    const bottom = `╚${'═'.repeat(longestName + 2)}╩${'═'.repeat(8)}╝`
+    const header = createRow([
+      formatName('Method'),
+      chalk.bold('Size'.padStart(6))
+    ])
+    const content = methods.map(i => i.border).join('\n')
 
-const legend = () =>
-  `║ ${chalk.bold('Method'.padEnd(15))}║${chalk.bold('Size'.padStart(7))} ║`
-
-const content = m =>
-  `║ ${m.name.padEnd(15)}║${chalk[m.size > 1024 ? 'red' : 'green'](
-    `${m.size}`.padStart(5)
-  )} B ║\n`
-
-const log = data => wrapper(25, data.map(content).join(''))
-
-const args = process.argv.slice(2)
-
-const libDir = path.resolve('lib')
-const methods = args.length
-  ? args
-  : fs.readdirSync(libDir).filter(i => i !== 'index.js')
-
-Promise.all(methods.map(getSizeAndSave)).then(sizes => {
-  log(sizes.sort(alphabetically))
-  const over = sizes.filter(i => i.size > 1024)
-  if (over.length) {
-    const longestName = over
-      .map(i => i.name)
-      .reduce((max, cur) => (cur > max ? cur : max), 0)
-    console.log('')
-    console.log(
-      chalk.bold.white.bgRed(' ERR '),
-      chalk.red(
-        `You have ${over.length} method${over.length > 1 ? 's' : ''} over 1KB`
-      )
-    )
-    console.log(
-      over.map(i => `${i.name.padEnd(longestName)}: ${i.size} B`).join('\n'),
-      '\n'
-    )
-    process.exit(1)
-  }
-})
+    console.log([topper, header, hrline, content, bottom].join('\n'))
+    return methods
+  })
+  // Draw list of failed methods
+  .then(methods => {
+    const failed = methods
+      .filter(i => i.size > 1024)
+      .map(i => `${formatName(i.name)}: ${i.size} B`)
+    if (failed > 0) {
+      console.log(`There are over-sized methods:\n${failed.join('\n')}`)
+      process.exit(1)
+    }
+  })
+  // Log errors
+  .catch(err => {
+    if (Array.isArray(err)) {
+      switch (err[0]) {
+        case 'NO_METHODS':
+          console.log('No methods matched')
+          break
+        default:
+          console.log('Unknown error happened')
+      }
+    }
+  })
