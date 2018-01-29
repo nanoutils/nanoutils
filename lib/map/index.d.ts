0a9674d47957bc5d26215080fdfb9f34f1afc6e3
Alexey_Berezin <alexey_berezin@epam.com>
2018-01-30 00:05:47 +0300

    Edited map, size is 294B, added test, types

--- lib/map/index.d.ts
+++ lib/map/index.d.ts
@@ -1,6 +1,7 @@
 import { 
   CurriedFunction1,
   CurriedFunction2,
+  Functor,
 } from '../../misc/types/fn';
 
-export default function map<T1, T2>(): CurriedFunction2mapper<CurriedFunction1<T1, T2>, T1[], T2[]>;
+export default function map<T1, T2>(): CurriedFunction2<CurriedFunction1<T1, T2>, Functor<T1>, Functor<T2>>;
