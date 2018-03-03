6825c8b082c79ca5f7e5171eb9cdcbd0b0cd0eca
Alexey_Berezin <alexey_berezin@epam.com>
2018-03-03 09:44:18 +0300

    Put all calculations to function `time`

--- misc/cli/time.js
+++ misc/cli/time.js
@@ -8,13 +8,6 @@ const readdir = util.promisify(fs.readdir)
 const writeFile = util.promisify(fs.writeFile)
 const args = minimist(process.argv.slice(2))
 
-function getNanoutilsFunction(name) {
-  return require(path.resolve('cjs', name, 'index.js'))
-}
-function getRamdaFunction(name) {
-  return require(path.resolve('node_modules/ramda/src', `${name}.js`))
-}
-
 const execute = (f, { args, n }) => {
   let time = 0
   let results = []
@@ -43,59 +36,17 @@ const multipleCompare = (f1, f2, { argss, options }) => {
   }
   return times
 }
-
-// TODO: to complete for all functions
-async function time() {
-  try {
-    // Get lib path
-    const lib = path.resolve('cjs')
-    // Read all methods
-    let methods = await readdir(lib)
-    // Methods without internal helpers
-    methods = methods.filter(m => m !== '_internal')
-    // Chosen methods from args
-    methods = args._.length
-      ? methods.filter(m => args._.includes(m))
-      : methods
-    // Interrupt without methods
-    if (!methods.length) {
-      throw new TypeError('No methods matched')
-    }
-    // Check time
-    
-    console.log(JSON.stringify(methods))
-  } catch (error) {
-    console.log(error)
-  }
-}
-
 const getTimes = (name, n = 1000) => {
-  const nuFunction = getNanoutilsFunction(name)
-  const ramdaFunction = getRamdaFunction(name)
+  const nuFunction = require(path.resolve('cjs', name, 'index.js'))
+  const ramdaFunction = require(path.resolve('node_modules/ramda/src', `${name}.js`))
   const getTime = require(path.resolve('lib', name, `${name}.performance.js`))
   const argss = getTime(nuFunction, ramdaFunction)
 
   const expected = argss.map(args => ramdaFunction(...args))
   const received = multipleCompare(nuFunction, ramdaFunction, { argss, n })
   
-  return { time: received.map(([ r1, r2 ]) => [ r1.time, r2.time ]) }
+  return received.map(([ r1, r2 ]) => [ r1.time, r2.time ])
 }
-
-const header = '## Nanoutils method\'s time'
-const table = [
-  ['Method', 'Lib', '0%', '50%', '100%'],
-  ...[getTimes('union')].reduce((acc, { time }) => {
-    const nano = time.map(([ t ]) => t.toFixed(2) + 'ms')
-    const ramda = time.map(([ _, t ]) => t.toFixed(2) + 'ms')
-    const diff = time.map(([ t1, t2 ]) => (t1 > t2 ? '+' : '') + (t1 - t2).toFixed(2) + 'ms')
-    return [
-      ...acc, 
-      ['union', 'nano', ...nano],
-      ['', 'ramda', ...ramda],
-      ['', 'diff', ...diff],
-    ]
-  }, [])
-]
 
 const countTable = table => {
   const rowN = table.length
@@ -128,9 +79,57 @@ const countTable = table => {
   return str
 }
 
-console.log([header, countTable(table)].join('\n'))
+// TODO: to complete for all functions
+async function time() {
+  try {
+    // Get lib path
+    const lib = path.resolve('cjs')
+    // Read all methods
+    let methods = await readdir(lib)
+    // Methods without internal helpers
+    methods = methods.filter(m => m !== '_internal')
+    // Chosen methods from args
+    methods = args._.length
+      ? methods.filter(m => args._.includes(m))
+      : methods
+    // Interrupt without methods
+    if (!methods.length) {
+      throw new TypeError('No methods matched')
+    }
+    // Check time
+    methods = methods.map(m => ({
+      name: m,
+      time: getTimes(m)
+    }))
+    // Sort alphabetically
+    methods.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
+    // Create table
+    const header = '## Nanoutils method\'s time'
+    const table = [
+      ['Method', 'Lib', '0%', '50%', '100%'],
+      ...methods.map(({ time }) => time).reduce((acc, time) => {
+        const nano = time.map(([ t ]) => t.toFixed(2) + 'ms')
+        const ramda = time.map(([ _, t ]) => t.toFixed(2) + 'ms')
+        const diff = time.map(([ t1, t2 ]) => (t1 > t2 ? '+' : '') + (t1 - t2).toFixed(2) + 'ms')
+        return [
+          ...acc, 
+          ['union', 'nano', ...nano],
+          ['', 'ramda', ...ramda],
+          ['', 'diff', ...diff],
+        ]
+      }, [])
+    ]
+    const drawing = [header, countTable(table)].join('\n')
+    // Draw it in console
+    console.log(drawing)
+    // write to file
+    writeFile(
+      'TIMES.md',
+      [header, countTable(table)].join('\n')
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-writeFile(
-  'TIMES.md',
-  [header, countTable(table)].join('\n')
-)
+time()
